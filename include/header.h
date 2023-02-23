2dccc59c35cd1945530d18c05b7efb66610a495d
InfinitePain <emirhangoktas01@gmail.com>
2023-02-23 14:33:00 +0100

    Some project necessary files added

---
+++ include/header.h
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : header.h
+ Author      : InfinitePain
+ License     : Apache License 2.0
+ Description : Header file for Snake game in C
+ ============================================================================
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <stdio.h>
+
+void hello_world() {
+	puts("!!!Hello World!!!");
+}
+
+#endif /* HEADER_H_ */
---
+++ src/game.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : game.c
+ Author      : InfinitePain
+ Version     : 0.1.0
+ License     : Apache License 2.0
+ Description : Snake game in C
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+int main(void) {
+    hello_world();
+    getchar();
+    return EXIT_SUCCESS;
+}
