3b551a7c76f78e242fbff841d3d46fd2b6e916ac
Emirhan Göktas <emirhangoktas01@gmail.com>
2023-05-26 00:48:17 +0200

    feat: Add app_status.c with game state variables - New app_status.c file added - Defines GAME_STATE and GAME_MODE variables

---
+++ src/app_status.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : app_status.c
+ Author      : InfinitePain
+ License     : Apache License 2.0
+ Description : Source file for Snake-by-InfinitePain
+=============================================================================
+*/
+
+#include "app_status.h"
+
+
+GameState GAME_STATE = NOT_STARTED;
+GameMode GAME_MODE = NOT_SELECTED;
