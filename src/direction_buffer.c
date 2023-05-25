0b64974b0b47a8fdca6c0573029fa60f1f2778dd
Emirhan Göktas <emirhangoktas01@gmail.com>
2023-05-26 00:56:32 +0200

    Improve error handling in direction_buffer.c
    - Added checks for the GAME_STATE variable in all functions to prevent execution in case of a critical error.

--- src/direction_buffer.c
+++ src/direction_buffer.c
@@ -8,29 +8,39 @@
 */
 
 #include "direction_buffer.h"
+#include "app_status.h"
 
-void buffer_init(DirectionBuffer *buffer) {
-    buffer->head = 0;
+void buffer_init(DirectionBuffer* buffer) {
+	if (GAME_STATE == CRITICAL_ERROR) {
+		return;
+	}
+	buffer->head = 0;
     buffer->tail = 0;
 }
 
-bool buffer_is_empty(DirectionBuffer *buffer) {
-    return buffer->head == buffer->tail;
+bool buffer_is_empty(DirectionBuffer* buffer) {
+	return buffer->head == buffer->tail;
 }
 
 bool buffer_is_full(DirectionBuffer *buffer) {
     return (buffer->head + 1) % BUFFER_SIZE == buffer->tail;
 }
 
-void buffer_push(DirectionBuffer *buffer, int direction) {
-    if (!buffer_is_full(buffer)) {
+void buffer_push(DirectionBuffer* buffer, int direction) {
+	if (GAME_STATE == CRITICAL_ERROR) {
+		return;
+	}
+	if (!buffer_is_full(buffer)) {
         buffer->buffer[buffer->head] = direction;
         buffer->head = (buffer->head + 1) % BUFFER_SIZE;
     }
 }
 
-int buffer_pop(DirectionBuffer *buffer) {
-    if (!buffer_is_empty(buffer)) {
+int buffer_pop(DirectionBuffer* buffer) {
+	if (GAME_STATE == CRITICAL_ERROR) {
+		return -1;
+	}
+	if (!buffer_is_empty(buffer)) {
         int direction = buffer->buffer[buffer->tail];
         buffer->tail = (buffer->tail + 1) % BUFFER_SIZE;
         return direction;
@@ -39,13 +49,19 @@ int buffer_pop(DirectionBuffer *buffer) {
 }
 
 int buffer_peek(DirectionBuffer* buffer) {
-    if (buffer_is_empty(buffer)) {
+	if (GAME_STATE == CRITICAL_ERROR) {
+		return -1;
+	}
+	if (buffer_is_empty(buffer)) {
         return -1;
     }
     return buffer->buffer[buffer->tail];
 }
 
-void buffer_clear(DirectionBuffer *buffer) {
-    buffer->head = 0;
+void buffer_clear(DirectionBuffer* buffer) {
+	if (GAME_STATE == CRITICAL_ERROR) {
+		return;
+	}
+	buffer->head = 0;
     buffer->tail = 0;
 }
